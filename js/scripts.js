3ec6e45c89110a48623edcf23bc6f9ff10ea2482
Misaki Koonce and Zubeyde Sisman <zubeyde@>
2019-08-14 13:43:50 -0700

    Make a value of result and insert in a div#output.

--- js/scripts.js
+++ js/scripts.js
@@ -21,7 +21,8 @@ $(document).ready(function(){
     event.preventDefault();
     var number1 = parseInt($("#add1").val());
     var number2 = parseInt($("#add2").val());
-    alert(add(number1, number2));
+    var result = add(number1, number2);
+    $("#output").text(result);
   })
 
 });
