ec71d2bca1d63fe65fb94e732d699d5e6b4b0e23
Misaki Koonce and Zubeyde Sisman <misaki@>
2019-08-14 13:36:57 -0700

    Add bootstrap and jQuery.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,27 @@
+// Back-end logic:
 var add = function(number1, number2) {
   return number1 + number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
 
-alert(add(number1, number2));
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+//Front-end logic:
+$(document).ready(function(){
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    alert(add(number1, number2));
+  })
+
+});
